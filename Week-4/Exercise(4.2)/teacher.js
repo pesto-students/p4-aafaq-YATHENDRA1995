2e7656f8a11ff0f781f81c1795e529103d9ebcf2
YATHENDRA <ny@rapidinnovation.dev>
2022-08-11 22:59:31 +0530

    Updated Exercise(4.2) with setPrototype method

--- Week-4/Exercise(4.2)/teacher.js
+++ Week-4/Exercise(4.2)/teacher.js
@@ -1,22 +1,41 @@
-let Person = function () { };
+// Approach 1
+
+// let Person = function () { };
+// Person.prototype.initialize = function (name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
+
+// class Teacher extends Person {
+//   constructor(name) {
+//     super(name)
+//   }
+//   teach(subject) {
+//     console.log(`${this.name} is now teaching ${subject}`);
+//   }
+// }
+
+// let him = new Teacher();
+// him.initialize('Adam', 45);
+// him.teach('inheritance')
+
+
+// Approach 2
+var Person = function () {};
 
 Person.prototype.initialize = function (name, age) {
   this.name = name;
   this.age = age;
-}
+};
 
+var Teacher = function () {};
+Object.setPrototypeOf(Teacher.prototype, Person.prototype);
 
-class Teacher extends Person {
-  constructor(name) {
-    super(name)
-  }
-  teach(subject) {
-    console.log(`${this.name} is now teaching ${subject}`);
-  }
-}
+Teacher.prototype.teach = function (subject) {
+  this.subject = subject;
+  console.log(`${this.name} is now teaching ${this.subject}`);
+};
 
-
-let him = new Teacher();
-
-him.initialize('Adam', 45);
-him.teach('inheritance')
+var him = new Teacher();
+him.initialize("Adam", 45);
+him.teach("Inheritance");
