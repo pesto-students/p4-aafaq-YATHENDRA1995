94b6c2812efd86583688f5282290dd63025d472c
YATHENDRA <ny@rapidinnovation.dev>
2022-08-24 13:19:50 +0530

    Completed Exercise6.1 - Max Sum Contiguous Sub Array

---
+++ Week-6/Exercise6.1/maxSumSubArray.js
@@ -0,0 +1,18 @@
+const maxSumSubArray = (array) => {
+  
+  let currentMaxSum = 0
+  let globalMaxSum = 0
+  
+  array.map((data) => {
+    currentMaxSum = Math.max(data, data + currentMaxSum)
+    globalMaxSum = Math.max(globalMaxSum, currentMaxSum)
+  })
+  
+  if (globalMaxSum === 0) return Math.max(...array)
+  
+  return globalMaxSum
+}
+
+console.log(maxSumSubArray([1,2,-1,4]))
+console.log(maxSumSubArray([-5, 4, 6, -3, 4, -1]))
+console.log(maxSumSubArray([-1, -2, -3]))
