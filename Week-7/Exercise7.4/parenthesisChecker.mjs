9953f1eb3bfd18305a91e164316716b51dd36b5a
YATHENDRA <ny@rapidinnovation.dev>
2022-09-01 21:10:11 +0530

    Implemented stack and completed Exercise7.4

---
+++ Week-7/Exercise7.4/parenthesisChecker.mjs
@@ -0,0 +1,49 @@
+import Stack from '../Utilities/stack.mjs'
+
+const parenthesisChecker = (str) => {
+
+  const stack = new Stack()
+
+  if (str.length === 0) return false
+
+  for (let i = 0; i < str.length; i++) {
+
+    if (str[i] === '{' || str[i] === '[' || str[i] === '(') {
+      stack.push(str[i])
+      continue;
+    } else if (str[i] === '}' || str[i] === ']' || str[i] === ')') {
+      if (stack.isEmpty()) return false
+      const temp = stack.pop()
+      switch (str[i]) {
+        case '}':
+          if (temp === '[' || temp === '(') return false;
+          break;
+        case ']':
+          if (temp === '{' || temp === '(') return false;
+          break;
+        case ')':
+          if (temp === '{' || temp === '[') return false;
+          break;
+      }
+    } else {
+      return false
+    }
+
+  }
+
+  return stack.isEmpty()
+
+}
+
+/*
+  Space Complexity - O(N)
+  Time Compltexity - O(N)
+*/
+
+console.log(parenthesisChecker('{}'))
+console.log(parenthesisChecker('{[()]}'))
+console.log(parenthesisChecker('{'))
+console.log(parenthesisChecker('abv'))
+console.log(parenthesisChecker('{yathendra}'))
+console.log(parenthesisChecker('{]'))
+console.log(parenthesisChecker('{}[]'))
